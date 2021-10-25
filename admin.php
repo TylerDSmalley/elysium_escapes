9fcb82c8720a17db2f2a5777d61024e70702eac2
Ileana528 <86622807+Ileana528@users.noreply.github.com>
2021-10-25 07:50:49 -0400

    added admin folder with starting template for user list and user edit

--- admin.php
+++ admin.php
@@ -4,5 +4,16 @@ require_once 'vendor/autoload.php';
 
 require_once 'init.php';
 
-// $app->get('/admin/user/list', function .....);
+ $app->get('/admin/users/list', function($request,$response,$args){
+    $userList = DB::query("SELECT * FROM users");
+    return $this->view->render($response,'admin/users_list.html.twig',['usersList'=> $userList]);
+ });
 
+$app->get('/admin/users/{id:[0-9]+}/edit',function($request,$response,$args){
+ $user = DB::queryFirstRow("SELECT * FROM users WHERE id=%i",$args['id']);
+ if(!$user){
+   $response = $response->withStatus(404);
+   return $this->view->render($response,'admin/not_found.html.twig');
+ }
+   return $this->view->render($response,'admin/users_edit.html.twig',['user'=> $user]);
+});
