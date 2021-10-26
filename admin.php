2b03ae3db2e8ba06a85ab235d837f5aa635b97c8
Tyler <TylerDSmalley@gmail.com>
2021-10-26 14:21:41 -0400

    Update admin.php
    added logic to add destinations to DB

--- admin.php
+++ admin.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'vendor/autoload.php';
-
+require_once 'utils.php';
 require_once 'init.php';
 
  $app->get('/admin/users/list', function($request,$response,$args){
@@ -22,7 +22,79 @@ $app->get('/error_internal', function ($request, $response, $args) {
   return $this->view->render($response, 'error_internal.html.twig');
 });
 
-
+// ADD DESTINATION HANDLER
+//State 1 - display form
 $app->get('/admin/destinations', function($request,$response,$args){
   return $this->view->render($response,'admin/destinations_add.html.twig');
+});
+
+//STATE 2 & 3 = recieving submission
+$app->post('/admin/destinations', function ($request, $response, $args) use ($log) {
+
+  $destination_description = $destination_name = $photo =  "";
+  $errors = array('destination_name' => '', 'destination_description' => '', 'photo' => '');
+
+  // check destination_name
+  if (empty($request->getParam('destination_name'))) {
+      $errors['destination_name'] = 'A Seller Name is required';
+  } else {
+      $destination_name = $request->getParam('destination_name');
+      if (strlen($destination_name) < 2 || strlen($destination_name) > 50) {
+          $errors['destination_name'] = 'destination_name must be 2-50 characters long';
+      } else {
+          $finaldestination_name = htmlentities($destination_name);
+      }
+  }
+
+  // check destination_description
+  if (empty($request->getParam('destination_description'))) {
+      $errors['destination_description'] = 'An Item Description is required';
+  } else {
+      $destination_description = $request->getParam('destination_description');
+      if (strlen($destination_description) < 2 || strlen($destination_description) > 5000) {
+          $errors['itemDescription'] = 'Body must be 2-10000 characters long';
+      } else {
+          $final_destination_description = strip_tags($destination_description, "<p><ul><li><em><strong><i><b><ol><h3><h4><h5><span><pre>");
+          $final_destination_description = htmlentities($final_destination_description);
+      }
+  }
+
+  // check photo
+  $photo = $_FILES['photo'];
+  if ($photo == null) {
+      $errors['photo'] = 'A photo is required';
+  } else {
+      $photoFilePath = "";
+      $retval = verifyUploadedPhoto($photoFilePath, $destination_name);
+      if ($retval !== TRUE) {
+          $errors['photo'] = $retval; // string with error was returned, add it to error list
+      }
+  }
+
+
+  if (array_filter($errors)) { //STATE 2 = errors
+      $valuesList = ['destination_name' => $destination_name, 'destination_description' => $destination_description, 'photo' => $photoFilePath];
+      return $this->view->render($response, 'admin/destinations_add.html.twig', ['errors' => $errors, 'v' => $valuesList]);
+  } else {
+      // STATE 3: submission successful
+      // insert the record and inform user
+
+      // 1. move uploaded file to its desired location
+      if (!move_uploaded_file($_FILES['photo']['tmp_name'], $photoFilePath)) {
+          die("Error moving the uploaded file. Action aborted.");
+      }
+      // 2. insert a new record with file path
+      $finalFilePath = htmlentities($photoFilePath);
+
+      //save to db and check
+      DB::insert('destinations', [
+          'destination_name' => $finaldestination_name,
+          'destination_description' => $final_destination_description,
+          'destination_imagepath' => $finalFilePath,
+      ]);
+
+      $log->debug(sprintf("new auction created with id=%s", $_SERVER['REMOTE_ADDR'], DB::insertId())); //needs test
+      return $this->view->render($response, 'admin/destinations_add.html.twig'); //needs confirmation signal
+  } // end POST check
+
 });
