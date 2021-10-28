72e5f5b2e805c462ec1f059d2bd55377d764932d
Stefan Needham <2095758@johnabbottcollege.net>
2021-10-27 22:03:28 -0400

    test booking page added

--- index.php
+++ index.php
@@ -62,4 +62,80 @@ if(preg_match('/^[\.a-zA-Z0-9,!? ]*$/',$firstName) != 1 || strlen($firstName) < 
    }
 });
 
+$app->get('/testbooking', function ($request, $response, $args) {
+  return $this->view->render($response, 'testbooking.html.twig');
+});
+
+
+$app->post('/testbooking', function ($request, $response, $args) {
+  $location = $request->getParam('location');
+  $adults = $request->getParam('adults');
+  $children = $request->getParam('children');
+  $arrival = $request->getParam('arrival');
+  $departure = $request->getParam('departure');
+  $hotelList = searchHotels($location, $adults, $children, $arrival, $departure);
+  return $this->view->render($response, 'apitestbooking.html.twig', ['location' => $location, 'adults' => $adults, 'children' => $children, 'arrival' => $arrival, 'departure' => $departure, 'h' => $hotelList->result]);
+});
+
+function callAPI($url) {
+$curl = curl_init($url);
+
+curl_setopt_array($curl, [
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_SSL_VERIFYPEER => FALSE,
+  CURLOPT_SSL_VERIFYHOST => FALSE,
+      CURLOPT_HTTPHEADER => [
+          "x-rapidapi-host: booking-com.p.rapidapi.com",
+          "x-rapidapi-key: ddadb2b982msh2f5bdc8948a96e5p1d52b9jsn9d517fe644de"
+      ],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+$data = json_decode ( $response);
+
+return $data;
+}
+
+function searchHotels($location, $adults, $children, $arrival, $departure) {
+  switch ($location) {
+      case "Bermuda":
+          $locationId = 24;
+          break;
+      case "Fiji":
+          $locationId = 71;
+          break;
+      case "Mauritius":
+          $locationId = 135;
+          break;
+  }
+
+  // $destinationType = "country"; // get list
+  // $orderBy = "popularity"; // get list
+  // $numberOfRooms = 1;
+  $numberOfChildren = 1;
+  $childrenAges = urlencode("5,0"); // %2C == , // "5%2C0" // urlencode("5,0")
+
+  $apiUrl = "https://booking-com.p.rapidapi.com/v1/hotels/search?"
+  ."dest_type=country"
+  ."&checkin_date=" . $arrival
+  ."&room_number=1"
+  ."&checkout_date=" . $departure
+  ."&order_by=popularity"
+  ."&dest_id=" . $locationId
+  ."&adults_number=" . $adults
+  ."&units=metric"
+  ."&filter_by_currency=CAD"
+  ."&locale=en-us"
+  ."&children_ages=" . $childrenAges
+  ."&include_adjacency=false"
+  ."&children_number=" . $numberOfChildren;
+
+  return callAPI($apiUrl);   
+  
+}
+
 $app->run();
