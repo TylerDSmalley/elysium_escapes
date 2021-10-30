27cd8bb72c05c95044895b1ade46d00fcd924396
Tyler <TylerDSmalley@gmail.com>
2021-10-29 23:52:00 -0400

    Create session.php
    added for debug purposes

---
+++ session.php
@@ -0,0 +1,16 @@
+<?php
+    echo "<pre>\n";
+    session_start();
+
+
+   //  $_SESSION['blogUser'] = array("id"  => 5, 'name' => 'Jerry B.', 'email' => 'jerry@jerry.com');
+
+    print_r($_SESSION);
+    echo "\n";
+
+    // is someone logged in?
+    if (isset($_SESSION['user'])) {
+        echo "you're logged in as " . $_SESSION['email']['account_type'];
+    } else {
+        echo 'not logged in';
+    }
