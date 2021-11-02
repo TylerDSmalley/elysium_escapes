ec62d80a62babd0613b7c8267c5b90aa23148a1f
Stefan Needham <2095758@johnabbottcollege.net>
2021-11-01 22:22:38 -0400

    update search hotels function

--- index.php
+++ index.php
@@ -59,49 +59,62 @@ return $data;
 }
 
 function searchHotels($location, $adults, $children, &$childrenAges, $arrival, $departure) {
-  switch ($location) {
-      case "Bermuda":
-          $locationId = 24;
-          break;
-      case "Fiji":
-          $locationId = 71;
-          break;
-      case "Mauritius":
-          $locationId = 135;
-          break;
-  }
+    $destinationType = "country";
+    switch ($location) {
+        case "Bahamas":
+            $locationId = 16;
+            break;
+        case "Bora Bora":
+            $destinationType = "city";
+            $locationId = 900054086;
+            break;
+        case "Phuket":
+            $destinationType = "city";
+            $locationId = -3253342;
+            break;
+            
+        case "Bermuda":
+            $locationId = 24;
+            break;
+        case "Fiji":
+            $locationId = 71;
+            break;
+        case "Mauritius":
+            $locationId = 135;
+            break;
+    }
 
-  // $destinationType = "country"; // get list
-  // $orderBy = "popularity"; // get list
-  // $numberOfRooms = 1;
+    // $destinationType = "country"; // get list
+    // $orderBy = "popularity"; // get list
+    // $numberOfRooms = 1;
 
-  $apiUrl = "https://booking-com.p.rapidapi.com/v1/hotels/search?"
-  ."dest_type=country"
-  ."&checkin_date=" . $arrival
-  ."&room_number=1"
-  ."&checkout_date=" . $departure
-  ."&order_by=popularity"
-  ."&dest_id=" . $locationId
-  ."&adults_number=" . $adults
-  ."&units=metric"
-  ."&filter_by_currency=CAD"
-  ."&locale=en-us"
-  ."&include_adjacency=false";
-  
-  if ($children > 0) {
-      $apiUrl = $apiUrl . "&children_number=" . $children . "&children_ages=";
-      for ($i = $children; $i >= 1; $i--) {
-          $apiUrl = $apiUrl . "8";
-          $childrenAges = $childrenAges . "8";
-          if ($i > 1) {
-              $apiUrl = $apiUrl . "%2C";
-              $childrenAges = $childrenAges . ",";
-          }
-      }
-  }
+    $apiUrl = "https://booking-com.p.rapidapi.com/v1/hotels/search?"
+    ."dest_type=" . $destinationType
+    ."&checkin_date=" . $arrival
+    ."&room_number=1"
+    ."&checkout_date=" . $departure
+    ."&order_by=popularity"
+    ."&dest_id=" . $locationId
+    ."&adults_number=" . $adults
+    ."&units=metric"
+    ."&filter_by_currency=CAD"
+    ."&locale=en-us"
+    ."&include_adjacency=false";
+    
+    if ($children > 0) {
+        $apiUrl = $apiUrl . "&children_number=" . $children . "&children_ages=";
+        for ($i = $children; $i >= 1; $i--) {
+            $apiUrl = $apiUrl . "8";
+            $childrenAges = $childrenAges . "8";
+            if ($i > 1) {
+                $apiUrl = $apiUrl . "%2C";
+                $childrenAges = $childrenAges . ",";
+            }
+        }
+    }
 
-  return callAPI($apiUrl);   
-  
+    return callAPI($apiUrl);
+    
 }
 
 $app->run();
