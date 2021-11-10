8c5b3ee0434119e5d9ee7e4f4790123615d6280c
Tyler <TylerDSmalley@gmail.com>
2021-11-10 14:12:02 -0500

    Update nav.js
    fixed image filepath

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,9 +1,9 @@
 function openNav() {
     var Image_Id = document.getElementById('mobile-nav-button');
-    if (Image_Id.src.match("images/hamburger.png")) {
-        Image_Id.src = "images/crossed.png";
+    if (Image_Id.src.match("../images/hamburger.png")) {
+        Image_Id.src = "../images/crossed.png";
     } else {
-        Image_Id.src = "images/hamburger.png";
+        Image_Id.src = "../images/hamburger.png";
     }
 
     if (document.getElementsByClassName("nav")[0].style.left === '100%' || document.getElementsByClassName("nav")[0].style.left === '') {
