be4d883d8b7750fd0d7b43f7bc9c6a166fae31b4
Stefan Needham <2095758@johnabbottcollege.net>
2021-11-12 21:08:35 -0500

    update index

--- index.php
+++ index.php
@@ -121,8 +121,8 @@ $app->post('/create', function ($request, $response, $args) {
         // Replace this constant with a calculation of the order's amount
         // Calculate the order total on the server to prevent
         // people from directly manipulating the amount on the client
-        // return 1400;
-        return $items[0]["total"];
+        return 1400;
+        // return $items[0]["total"];
     }
 
 
